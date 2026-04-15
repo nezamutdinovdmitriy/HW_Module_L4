a598edb766bd574709c5ba74424e3ceae9dcd729
nezamutdinovdmitriy <nezamutdinovdmitriy@gmail.com>
2026-04-15 19:20:25 +0500

    Update EntryPointSceneAutoLoader.cs

--- Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
+++ Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
@@ -6,12 +7,44 @@ namespace Assets._Project.Develop.Editor
     [InitializeOnLoad]
     public static class EntryPointSceneAutoLoader
     {
+        private const string MenuPath = "PlayFromBootstrap/Enabled";
+        private const string PlayFromBootstrapKey = "PlayFromBootstrapKey";
+
         static EntryPointSceneAutoLoader()
         {
-            if (EditorBuildSettings.scenes.Length == 0)
-                return;
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
 
-            EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if(state == PlayModeStateChange.ExitingPlayMode)
+            {
+                if(EditorPrefs.GetBool(PlayFromBootstrapKey) == false)
+                {
+                    EditorSceneManager.playModeStartScene = null;
+                    return;
+                }
+
+                if (EditorBuildSettings.scenes.Length == 0)
+                    return;
+
+                EditorSceneManager.playModeStartScene = AssetDatabase
+                    .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+            }
+        }
+
+        [MenuItem(MenuPath)]
+        private static void Toggle()
+        {
+            bool result = EditorPrefs.GetBool(PlayFromBootstrapKey);
+            EditorPrefs.SetBool(PlayFromBootstrapKey, !result);
+        }
+
+        [MenuItem(MenuPath, true)]
+        private static bool ToggleValidate()
+        {
+            Menu.SetChecked(MenuPath, EditorPrefs.GetBool(PlayFromBootstrapKey));
+            return true;
         }
     }
 }
